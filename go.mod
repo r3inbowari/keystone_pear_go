965d8d8456f54c83e5ff0bc799356b5bcd2cc848
r3inbowari <atuk@qq.com>
2020-10-23 17:55:07 +0800

    update

--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.14
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
 	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/gorilla/mux v1.8.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.7.0
-	golang.org/x/net v0.0.0-20201022231255-08b38378de70 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20201022231255-08b38378de70
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/gorilla/mux"
+	mgo "gopkg.in/mgo.v2"
+	"golang.org/x/net/context"
 	"net/http"
 )
 
@@ -14,9 +17,24 @@ type Version struct {
 	Patch int    `json:"patch"`
 }
 
-var c mqtt.Client
+var client mqtt.Client
+
+func GetMongoDB() (*mgo.Database, error) {
+	host := "mongodb://localhost:27017"
+	dbName := "learn_mongodb_golang"
+	session, err := mgo.Dial(host)
+	if err != nil {
+		return nil, err
+	}
+	db := session.DB(dbName)
+	return db, nil
+}
+
+var db *mgo.Database
 
 func main() {
+	db, _ = GetMongoDB()
+
 	opts := mqtt.NewClientOptions().AddBroker("tcp://r3inbowari.top:1883").SetClientID("sample_go")
 	opts.SetUsername("r3inb")
 	opts.SetPassword("159463")
@@ -26,16 +44,68 @@ func main() {
 	}
 
 	if token := c.Subscribe("meshNetwork/from/rootNode/checkupdate", 0, updateCallback); token.Wait() && token.Error() != nil {
-
+	}
+	if token := c.Subscribe("meshNetwork/from/rootNode/bme", 0, bmeCallback); token.Wait() && token.Error() != nil {
+	}
+	if token := c.Subscribe("meshNetwork/from/rootNode/apds", 0, apdsCallback); token.Wait() && token.Error() != nil {
+	}
+	if token := c.Subscribe("meshNetwork/from/rootNode/logon", 0, lopgonCallback); token.Wait() && token.Error() != nil {
 	}
 
-	http.HandleFunc("/check_update", HandleVersion)
-	log.Fatal(http.ListenAndServe(":2999", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/check_update", HandleVersion)
+	r.HandleFunc("/bme/{id}", HandleBME)
+	log.Fatal(http.ListenAndServe(":2999", r))
 }
 
 type Result struct {
 	ID string `json:"id"`
 }
+
+type MessageBody struct {
+	ID        string `json:"id"`
+	MID       string `json:"mid"`
+	Ts        int    `json:"ts"`
+	Operation int    `json:"operation"`
+	Data      Data   `json:"data"`
+}
+
+type Data struct {
+	Type    int       `json:"type"`
+	Measure []float64 `json:"measure"`
+}
+
+func bmeCallback(client mqtt.Client, msg mqtt.Message) {
+
+	var result MessageBody
+	_ = json.Unmarshal(msg.Payload(), &result)
+	insertResult, err := db.InsertOne(context.TODO(), result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[INFO] Topic ->", msg.Topic(), "Inserted BME Data ->", insertResult.InsertedID)
+}
+
+func apdsCallback(client mqtt.Client, msg mqtt.Message) {
+	var result MessageBody
+	_ = json.Unmarshal(msg.Payload(), &result)
+	insertResult, err := apdsConn.InsertOne(context.TODO(), result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[INFO] Topic ->", msg.Topic(), "Inserted APDS Data ->", insertResult.InsertedID)
+}
+
+func lopgonCallback(client mqtt.Client, msg mqtt.Message) {
+	var result MessageBody
+	_ = json.Unmarshal(msg.Payload(), &result)
+	insertResult, err := logonConn.InsertOne(context.TODO(), result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[INFO] Topic ->", msg.Topic(), "Inserted Logon Data ->", insertResult.InsertedID, "ID ->", result.ID)
+}
+
 func updateCallback(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("TOPIC: %s\n", msg.Topic())
 	fmt.Printf("MSG: %s\n", msg.Payload())
@@ -46,10 +116,46 @@ func updateCallback(client mqtt.Client, msg mqtt.Message) {
 		Major: GetConfig().IotVersion.A,
 		Minor: GetConfig().IotVersion.B,
 		Patch: GetConfig().IotVersion.C,
-		ID: result.ID,
+		ID:    result.ID,
 	}
 	ret, _ := json.Marshal(version)
 	client.Publish("meshNetwork/to/rootNode/checkupdate", 1, false, ret)
+}
+
+func HandleBME(w http.ResponseWriter, r *http.Request) {
+	var result MessageBody
+	vars := mux.Vars(r)
+
+	println(vars["id"])
+
+	pipeline := []bson.M{
+		{
+			"$group": bson.M{
+				"_id": "",
+				"max": bson.M{"$max": "$price"},
+			},
+		},
+	}
+	result := []bson.M{}
+	err = db.C("product").Pipe(pipeline).All(&result)
+
+
+	if err != nil {
+		_, _ = w.Write([]byte("{\"code\":233,\"msg\":\"not found data\"}"))
+	}
+	fmt.Printf("[INFO] Get BME value: %+v\n", result)
+	ret, _ := json.Marshal(result)
+	_, _ = w.Write(ret)
+}
+
+func HandleAPDS(w http.ResponseWriter, r *http.Request) {
+	version := Version{
+		Major: GetConfig().IotVersion.A,
+		Minor: GetConfig().IotVersion.B,
+		Patch: GetConfig().IotVersion.C,
+	}
+	ret, _ := json.Marshal(version)
+	_, _ = w.Write(ret)
 }
 
 func HandleVersion(w http.ResponseWriter, r *http.Request) {
